d306c9c4729058ab51d6ced165bf1254dfd592ee
zedparman <58415963+zedparman@users.noreply.github.com>
2024-02-28 08:58:19 +0330

    Create Footer.jsx

---
+++ src/app/[locale]/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <footer className="border h-32 flex flex-col items-center justify-center gap-5">
+      <h1>Footer</h1>
+      <p>2024 LogikMeter &#169;</p>
+    </footer>
+  );
+};
+
+export default FooterComponent;
