b12b07f5a3e5b1d102be55dae8ef66e51cdf55cc
zedparman <58415963+zedparman@users.noreply.github.com>
2024-02-28 08:57:31 +0330

    Create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,59 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+// const hre = require("hardhat");
+
+// async function main() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const unlockTime = currentTimestampInSeconds + 60;
+
+//   const lockedAmount = hre.ethers.parseEther("0.001");
+
+//   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+//     value: lockedAmount,
+//   });
+
+//   await lock.waitForDeployment();
+
+//   console.log(
+//     `Lock with ${ethers.formatEther(
+//       lockedAmount
+//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+//   );
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
+const { ethers } = require("hardhat");
+const fs = require("fs");
+
+async function main() {
+  // const contract_name = "DappVotes";
+  const Contract = await ethers.getContractFactory("DappVotes");
+  const contract = await Contract.deploy();
+
+  await contract.waitForDeployment();
+
+  console.log(contract);
+  const address = JSON.stringify({ address: contract.target }, null, 4);
+  fs.writeFile("./artifacts/contractAddress.json", address, "utf8", (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Deployed contract address", contract.target);
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
