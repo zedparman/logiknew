85299a2075fe9873b594eb6a520b1ee45a01ac41
zedparman <58415963+zedparman@users.noreply.github.com>
2024-02-28 08:59:25 +0330

    Create route.js

---
+++ src/app/api/auth/getqu/route.js
@@ -0,0 +1,18 @@
+// import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import connectDb from "../../../../../utils/connectDB";
+import User from "@/models/User";
+
+export async function POST(req, res) {
+  const data = await req.json();
+  const { userEmail } = data;
+  await connectDb();
+
+  const user = await User.findOne({ email: userEmail });
+
+  return NextResponse.json({
+    status: "success",
+    message: "successfully connected!",
+    data: user.questions,
+  });
+}
