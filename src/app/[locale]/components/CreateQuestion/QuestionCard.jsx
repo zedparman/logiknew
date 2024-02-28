1b1cc2018cbd3f1ccdcd68303f93e0c5e25e72cd
zedparman <58415963+zedparman@users.noreply.github.com>
2024-02-28 08:58:10 +0330

    Create QuestionCard.jsx

---
+++ src/app/[locale]/components/CreateQuestion/QuestionCard.jsx
@@ -0,0 +1,10 @@
+// import { Button } from "@/components/ui/button";
+// import React from "react";
+
+// const QuestionCard = ({ title, desc, index, handle }) => {
+//   return (
+    
+//   );
+// };
+
+// export default QuestionCard;
