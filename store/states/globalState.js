3cbed9d3945e9fb7640fdcc5bda92b95cb7e9a92
zedparman <58415963+zedparman@users.noreply.github.com>
2024-02-28 09:00:57 +0330

    Create globalState.js

---
+++ store/states/globalState.js
@@ -0,0 +1,14 @@
+export const globalStates = {
+  wallet: "",
+  createModal: "scale-0",
+  updateModal: "scale-0",
+  deleteModal: "scale-0",
+  contestModal: "scale-0",
+  chatModal: "scale-0",
+  polls: [],
+  poll: null,
+  group: null,
+  contestants: [],
+  currentUser: null,
+  sesseion : "",
+};
